215ab1a0957c1c199dfa52e6435aea34133d2ee8
spences10 <spences10apps@gmail.com>
2018-04-04 17:49:28 +0100

    add types and resolvers

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
 const typeDefs = `
-type Query {
-  info: String!
-}
+  type Query {
+    info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
+  }
 `
 
-// 2
+let links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }
+]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: root => root.id,
+    description: root => root.description,
+    url: root => root.url
   }
 }
 
-// 3
 const server = new GraphQLServer({
   typeDefs,
   resolvers
