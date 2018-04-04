7240652e801115fc5664fc9816fc9bd8f8dcc68d
spences10 <spences10apps@gmail.com>
2018-04-04 19:06:20 +0100

    add mutation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const typeDefs = `
     feed: [Link!]!
   }
 
+  type Mutation {
+    post(url: String!, description: String!): Link!
+  }
+
   type Link {
     id: ID!
     description: String!
@@ -21,22 +25,31 @@ let links = [
   }
 ]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links
   },
-  Link: {
-    id: root => root.id,
-    description: root => root.description,
-    url: root => root.url
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      }
+      links.push(link)
+      return link
+    }
   }
 }
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 })
+
 server.start(() =>
   console.log(`Server is running on http://localhost:4000`)
 )
