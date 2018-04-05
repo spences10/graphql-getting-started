352f9bb7ac4418aa03e4b7b0f8d9556856bffff6
spences10 <spences10apps@gmail.com>
2018-04-05 16:21:21 +0100

    authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload')
+
 const typeDefs = `
   type Query {
     info: String!
@@ -19,25 +23,9 @@ const typeDefs = `
 `
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.db.query.links({}, info)
-    }
-  },
-  Mutation: {
-    post: (root, args, context, info) => {
-      return context.db.mutation.createLink(
-        {
-          data: {
-            url: args.url,
-            description: args.description
-          }
-        },
-        info
-      )
-    }
-  }
+  Query,
+  Mutation,
+  AuthPayload
 }
 
 const server = new GraphQLServer({
