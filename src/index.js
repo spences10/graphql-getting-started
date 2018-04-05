3f2c464e5459c66f0595bffd3a8b42c8339c4091
spences10 <spences10apps@gmail.com>
2018-04-05 11:53:21 +0100

    adding the database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
 
 const typeDefs = `
   type Query {
@@ -17,37 +18,41 @@ const typeDefs = `
   }
 `
 
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-]
-
-let idCount = links.length
-
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    }
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link)
-      return link
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink(
+        {
+          data: {
+            url: args.url,
+            description: args.description
+          }
+        },
+        info
+      )
     }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint:
+        'https://eu1.prisma.sh/public-arrowwarrior-573/hackernews-node/dev',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
 })
 
 server.start(() =>
